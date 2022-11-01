3cfd7d5d1138b198361728caef8dd8ab4fad3c2b
TSSniper349 <85919024+TSSniper349@users.noreply.github.com>
2022-11-01 22:37:32 +0700

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/nhap3/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.nhap3
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screens/quiz/components/question_card_2.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:quiz_app/controllers/question_controller.dart';
+import 'package:quiz_app/models/Questions.dart';
+
+import '../../../constants.dart';
+import 'option.dart';
+
+class QuestionCard2 extends StatelessWidget {
+  const QuestionCard2({
+    Key? key,
+    // it means we have to pass this
+    required this.question,
+  }) : super(key: key);
+
+  final Question question;
+
+  @override
+  Widget build(BuildContext context) {
+    QuestionController _controller = Get.put(QuestionController());
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: kDefaultPadding),
+      padding: EdgeInsets.all(kDefaultPadding),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(25),
+      ),
+      child: Column(
+        children: [
+          Text(
+            question.question,
+            style: Theme.of(context)
+                .textTheme
+                .headline6
+                ?.copyWith(color: kBlackColor),
+          ),
+          SizedBox(height: kDefaultPadding * 2),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+            ...List.generate(
+              question.options.length,
+                  (index) => Option(
+                index: index,
+                text: question.options[index],
+                press: () => _controller.checkAns(question, index),
+              ),
+            ),
+          ],
+          ),
+        ],
+      ),
+    );
+  }
+}
